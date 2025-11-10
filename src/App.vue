614b9adbaf57ead17393e24c3884610c2c0cdee1
Greenca14 <124871992+Greenca14@users.noreply.github.com>
2025-11-10 16:46:07 +0500

    VueRouter

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,15 @@
   <div>
     <header>
       <nav>
+        <ul>
+          <li><router-link to="/">Главная</router-link></li>
+          <li><router-link to="/journals">Журналы</router-link></li>
+          <li><router-link to="/persons">Персоны</router-link></li>
+        </ul>
+        
         <div v-if="isAuthenticated && user">
-          Добро пожаловать, {{ user.name }}
-          <button @click="logout">Выйти</button>
+          Welcome, {{ user.name }}
+          <button @click="logout">Logout</button>
         </div>
         <div v-else>
           <form @submit.prevent="login">
@@ -17,74 +23,56 @@
               <input v-model="email" type="email" required />
             </div>
             <div>
-              <label>Пароль:</label>
+              <label>Password:</label>
               <input v-model="password" type="password" required />
             </div>
-            <button type="submit">Войти</button>
+            <button type="submit">Login</button>
             <p v-if="errorMessage" class="error">{{ errorMessage }}</p>
           </form>
         </div>
       </nav>
     </header>
-
-    <main>
-      <div v-if="isAuthenticated && user">
-        <h1>Система учета научных публикаций</h1>
-        <p>Email: {{ user.email }}</p>
-      </div>
-      <div v-else>
-        <h1>Система учета научных публикаций</h1>
-        <p>Требуется авторизация</p>
-      </div>
-    </main>
+    
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { useAuthStore } from './stores/authStore'
+import { useAuthStore } from './stores/authStore';
 
 export default {
   data() {
     return {
       email: '',
       password: '',
-      authStore: useAuthStore(),
-    }
+      authStore: useAuthStore()
+    };
   },
   computed: {
-    isAuthenticated() {
-      return this.authStore.isAuthenticated
-    },
-    user() {
-      return this.authStore.user
-    },
-    errorMessage() {
-      return this.authStore.errorMessage
-    },
+    isAuthenticated() { return this.authStore.isAuthenticated; },
+    user() { return this.authStore.user; },
+    errorMessage() { return this.authStore.errorMessage; }
   },
   methods: {
-    logout() {
-      this.authStore.logout()
-    },
-    login() {
+    logout() { this.authStore.logout(); },
+    login() { 
       this.authStore.login({
-        email: this.email,
-        password: this.password,
-      })
-    },
-  },
-  mounted() {
-    const token = localStorage.getItem('token')
-    if (token) {
-      this.authStore.isAuthenticated = true
-      this.authStore.getUser()
+        email: this.email, 
+        password: this.password
+      });
     }
   },
-}
+  mounted() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authStore.isAuthenticated = true;
+      this.authStore.getUser();
+    }
+  }
+};
 </script>
 
 <style>
-.error {
-  color: blue;
-}
+.error { color: red; }
+ul { display: flex; gap: 20px; list-style: none; }
 </style>
