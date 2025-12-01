89c2699d1d99fe05c841be5b5a22cf80242d28ed
Greenca14 <124871992+Greenca14@users.noreply.github.com>
2025-12-01 19:37:49 +0500

    Pagination

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
+const backendUrl = import.meta.env.VITE_BACKEND_URL;  
+
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     user: null,
